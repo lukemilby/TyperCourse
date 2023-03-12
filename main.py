42d57dd960e032c20e41b9a967ea1c7f364bc90c
Luke Milby <luke.milby@gmail.com>
2023-03-12 18:31:29 -0500

    start here

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
 import typer
 
-app = typer.Typer()
 
-
-@app.command()
-def pew(name: str = typer.Argument(..., help="Name of the person to Pew"), count: int = typer.Argument(1, help="How many times to pew")):
-    print(f"You {count * 'pew '}{name}")
-    print(f"RIP: {name}")
-
-
-@app.command()
-def slap(name: str = typer.Argument(..., help="Who to slap"),
-         size: str = typer.Argument("small", help="Size of hand to slap with")):
-    print(f"You slapped {name} with a {size} hand")
-    if size == "small":
-        print(f"{name}'s face is slightly red")
-    elif size == "medium":
-        print(f"{name}'s face is significantly red")
-    elif size == "large":
-        print(f"{name}'s knocked out")
-    else:
-        print(f"{name} ")
-
-
+def main(name: str):
+	print(f"Hello {name}")
 
 
 if __name__ == "__main__":
-    app()
+	typer.run(main)
