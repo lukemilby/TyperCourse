ceb6de8609a2ffff14afafd975e7cbf3a3311814
Luke Milby <luke.milby@gmail.com>
2023-03-17 16:25:21 -0500

    update lesson 2

--- Lesson-2/main.py
+++ Lesson-2/main.py
@@ -3,9 +3,13 @@ import typer
 app = typer.Typer()
 
 
+"""
+create a new command called pew that takes a name and a count for how many times to print pew
+use typer.Argument for count that sets a default and add the help parameter 
+"""
 @app.command()
-def pew(name: str = typer.Argument(..., help="Name of the person to Pew"), count: int = typer.Argument(1, help="How many times to pew")):
-    print(f"You {count * 'pew '}{name}")
+def pew(name:str, count:int = typer.Argument(1, help="how many times to pew")):
+    print(f"You {'pew ' * count}{name}")
 
 @app.command()
 def hello(name: str):
